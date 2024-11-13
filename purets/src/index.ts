92c29fa07a3fbf12935f12f6bd969dee89c9fdd7
Hrithik06 <el99el03@gmail.com>
2024-11-13 20:44:46 +0530

    Protected and Interfaces

--- purets/src/index.ts
+++ purets/src/index.ts
@@ -20,22 +20,82 @@
 // const hrithik = new User("h@h.com", "hrithik");
 // console.log(hrithik);
 
+// class User {
+//   private _courseCount = 1;
+//   readonly city: string = "NewYork";
+//   constructor(public email: string, private name: string) {}
+
+//   get courseContent(): number {
+//     return this._courseCount;
+//   }
+//   set courseContent(courseNum) {
+//     this._courseCount = courseNum;
+//   }
+
+//   get getAppleEmail(): string {
+//     return `apple${this.email}`;
+//   }
+// }
+// const hrithik = new User("h@h.com", "hrithik");
+// hrithik.courseContent = 5;
+// console.log(hrithik);
+
 class User {
-  private _courseCount = 1;
+  protected _courseCount = 1;
   readonly city: string = "NewYork";
   constructor(public email: string, private name: string) {}
 
   get courseContent(): number {
     return this._courseCount;
   }
-  set courseContent(courseNum) {
+  set courseContent(courseNum: number) {
     this._courseCount = courseNum;
   }
 
   get getAppleEmail(): string {
     return `apple${this.email}`;
   }
+  //Not accessible outside the class
+  private deleteToken() {
+    console.log("Token deleted");
+  }
 }
-const hrithik = new User("h@h.com", "hrithik");
-hrithik.courseContent = 5;
+class SubUser extends User {
+  isFamily: boolean = true;
+  set changeCourseCount(n: number) {
+    this._courseCount = 5;
+  }
+}
+const hrithik = new SubUser("h@h.com", "hrithik");
+hrithik.changeCourseCount = 15;
 console.log(hrithik);
+interface TakePhoto {
+  cameraMode: string;
+  filter: string;
+  burst: number;
+}
+
+class Instagram implements TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {}
+}
+interface Story {
+  createStory(): void;
+}
+class Youtube implements TakePhoto, Story {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number,
+    public short: string
+  ) {}
+  createStory(): void {
+    console.log("Story");
+  }
+}
+
+const yt = new Youtube("f", "e", 5, "d");
+console.log(yt);
